974354d1a708c175ac95489792d68d8e8715455d
Lucas Heitor <307839@aluno.unilins.edu.br>
2023-11-20 22:24:31 -0300

    Fixing Bug

--- app.py
+++ app.py
@@ -3,42 +3,42 @@ import pandas as pd
 import altair as alt
 from PIL import Image
 
-@st.cache_data
+@st.cache
 def gerar_df():
-    df = pd.read_excel(
+    dataframe = pd.read_excel(
         io="C:\\Users\\30783\\OneDrive\\Área de Trabalho\\python\\CouroXDolar.xlsx",
         engine="openpyxl",
         sheet_name="Planilha1",
         usecols="A:D",
         nrows=283
     )
-    return df
+    return dataframe
 
-df = gerar_df()
+df_global = gerar_df()
 
 colunasuteis = ['mes', 'ano', 'ValorCouro', 'ValorDolar']
-df = df[colunasuteis]
-df['ano'] = df['ano'].astype(str)
-df['mes'] = df['mes'].astype(str)
-df['mes'] = df['mes'].str.zfill(2)
-df['ano_mes'] = df['ano'] + '-' + df['mes']
+df_global = df_global[colunasuteis]
+df_global['ano'] = df_global['ano'].astype(str)
+df_global['mes'] = df_global['mes'].astype(str)
+df_global['mes'] = df_global['mes'].str.zfill(2)
+df_global['ano_mes'] = df_global['ano'] + '-' + df_global['mes']
 
 with st.sidebar:
     st.subheader('Precificação Couro x Dolar')
     logo_teste = Image.open("C:\\Users\\30783\\OneDrive\\Área de Trabalho\\python\\logo.jpg")
     st.image(logo_teste, use_column_width=True)
     st.subheader('Seleção de Filtros')
-    anos = ['Todos os anos'] + list(df['ano'].unique())
+    anos = ['Todos os anos'] + list(df_global['ano'].unique())
     ano_selecionado = st.selectbox('Escolha um ano:', anos)
 
     if ano_selecionado == 'Todos os anos':
-        df_filtrado = df
+        df_filtrado = df_global
     else:
-        df_filtrado = df[df['ano'] == ano_selecionado]
+        df_filtrado = df_global[df_global['ano'] == ano_selecionado]
 
-# Plotting the evolution of leather and dollar prices over the years
+
 chart_couro = alt.Chart(df_filtrado).mark_line().encode(
-    x='ano_mes',
+    x=alt.X('ano_mes:T', title='Ano e Mês'),
     y=alt.Y('ValorCouro', title='Preço do Couro'),
     tooltip=['ano_mes', 'ValorCouro']
 ).properties(
@@ -48,7 +48,7 @@ chart_couro = alt.Chart(df_filtrado).mark_line().encode(
 )
 
 chart_dolar = alt.Chart(df_filtrado).mark_bar().encode(
-    x='ano_mes',
+    x=alt.X('ano_mes:T', title='Ano e Mês'),
     y=alt.Y('ValorDolar', title='Preço do Dólar'),
     color=alt.value('orange'),
     tooltip=['ano_mes', 'ValorDolar']
@@ -58,7 +58,6 @@ chart_dolar = alt.Chart(df_filtrado).mark_bar().encode(
     title=f'Evolução do Preço do Dólar ({ano_selecionado if ano_selecionado != "Todos os anos" else "Todos os anos"})'
 )
 
-# Display the charts below each other using Streamlit
+
 st.altair_chart(chart_couro, use_container_width=True)
 st.altair_chart(chart_dolar, use_container_width=True)
-
